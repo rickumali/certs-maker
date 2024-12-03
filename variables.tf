0f0c7d6df5ac5aab495f705564825548ac73a915
Rick Umali <rickumali@gmail.com>
2024-12-02 21:04:59 -0500

    Brings in git_version and timestamp variables

--- variables.tf
+++ variables.tf
@@ -3,3 +3,13 @@ variable "cert_names" {
   type        = list(string)
   default     = ["web", "whoami", "httpbin"]
 }
+
+variable "git_version" {
+  description = "Branch and SHA1 ID of certs project"
+  type        = string
+}
+
+variable "timestamp" {
+  description = "YYYYMMDD-HHMM (24 hour time)"
+  type        = string
+}
